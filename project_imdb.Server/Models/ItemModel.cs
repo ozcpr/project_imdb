dfb011c87424400177773ce52e1c9402635f097d
Ozan Aydin <ozanium@outlook.com>
2024-01-07 12:32:26 +0300

    Tmdb api configuration

---
+++ project_imdb.Server/Models/ItemModel.cs
@@ -0,0 +1,21 @@
+﻿namespace project_imdb.Server.Models
+{
+    public class ItemModel
+    {
+        public int id { get; set; }
+
+        public string queryId { get; set; } = string.Empty; // query id for ex. movie. That means you search for a movie. If it was an actor, then it would be actor.
+
+        public string title { get; set; } = string.Empty;
+
+        public int year { get; set; }
+
+        public string description { get; set; } = string.Empty;
+
+        public string stars { get; set; } = string.Empty;
+
+        public string query { get; set; } = string.Empty;
+
+        public string imageUrl { get; set; } = string.Empty;
+    }
+}
--- project_imdb.client/src/App.jsx
+++ project_imdb.client/src/App.jsx
@@ -2,48 +2,73 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [forecasts, setForecasts] = useState();
+    const [data, setData] = useState();
 
     useEffect(() => {
-        populateWeatherData();
+        populateMovieData();
     }, []);
 
-    const contents = forecasts === undefined
+    const contents = data === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
+        : (
+            <table className="table table-striped" aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Rating</th>
+                        <th>Date</th>
+                        <th>Vote Count</th>
                     </tr>
-                )}
-            </tbody>
-        </table>;
+                </thead>
+                <tbody>
+                    {data.map(movie =>
+                        <tr key={movie.id}>
+                            <td>{movie.id}</td>
+                            <td>{movie.title}</td>
+                            <td>{movie.overview}</td>
+                            <td><img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} /></td>
+                            <td>{movie.vote_average}</td>
+                            <td>{movie.release_date}</td>
+                            <td>{movie.vote_count}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
+            <h1 id="tabelLabel">Top Movies</h1>
             <p>This component demonstrates fetching data from the server.</p>
             {contents}
         </div>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
+
+
+    async function populateMovieData() {
+        try {
+            const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200', {
+                headers: {
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzk2YmFjOWU2MGMxMTRkMWQwNzg4ZDUyMDg1Mjc5YSIsInN1YiI6IjY1OTllMDZlYzk5NWVlMDFmZWI0ZmFmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hQeZz4X41M1awI7VvpUGWn4Kg5T9I6USFP7QZ4gOjho',
+                    'accept': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            setData(data.results);
+        } catch (error) {
+            console.error('Error fetching movie data:', error);
+            // Handle the error as needed
+        }
     }
+
 }
 
 export default App;
